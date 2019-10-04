335e028d9c9913b5283b24a534cec7cda24613b2
Michele Chilanti <chilantim@gmail.com>
2019-10-04 11:26:39 -0500

    Build fixes

--- build.sh
+++ build.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-#Takes two parms:
+#Takes three parms:
 #CLI_VERSION
 #TRAVIS_TAG
+#CONTROLLER_VERSION
 set -e
-export CONTROLLER_BASE_URL := https://github.com/appsody/controller/releases/download/$CONTROLLER_VERSION
+export CONTROLLER_BASE_URL=https://github.com/appsody/controller/releases/download/$3
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker build -t $DOCKER_ORG/appsody-k8s:$2 -t $DOCKER_ORG/appsody-k8s:latest --build-arg CLI_VERSION=$1 --build-arg CONTROLLER_BASE_URL=${CONTROLLER_BASE_URL}.
+docker build -t $DOCKER_ORG/appsody-k8s:$2 -t $DOCKER_ORG/appsody-k8s:latest --build-arg CLI_VERSION=$1 --build-arg CONTROLLER_BASE_URL=${CONTROLLER_BASE_URL} .
 docker push $DOCKER_ORG/appsody-k8s
 docker push $DOCKER_ORG/appsody-k8s:$2
